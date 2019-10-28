4b23d0416143b6335145678c59c43b6606b01682
Bryan Villalon <bavillalon@gmail.com>
2019-10-28 12:56:37 -0500

    Updates

---
+++ precache-manifest.11a5722de658f276a028a3122419b270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d43c9b045f644d6322a950e8435596",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "3ab93f2b4e00ee9915aa",
+    "url": "/react-gh-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "99fcc92ca85cebd3d3e5",
+    "url": "/react-gh-pages/static/js/2.1da6d8e2.chunk.js"
+  },
+  {
+    "revision": "3ab93f2b4e00ee9915aa",
+    "url": "/react-gh-pages/static/js/main.58e05e5a.chunk.js"
+  },
+  {
+    "revision": "cb51794bc37f386c3121",
+    "url": "/react-gh-pages/static/js/runtime-main.5c2e6665.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
